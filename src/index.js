2afe75d7bcb24a3091d5fcdc51a418dc04e041db
강은빈 <kub0803@naver.com>
2021-06-30 11:29:58 +0900

    [MODIFY] code 수정

--- src/index.js
+++ src/index.js
@@ -16,12 +16,6 @@ const store = createStore(
 
 ReactDOM.render(
   <React.StrictMode>
-    {/* <Provider
-      store={createStoreWidthMiddleware(
-        window.__REDUX_DEVTOOLS_EXTENSION__ &&
-          window.__REDUX_DEVTOOLS_EXTENSION__()
-      )}
-    > */}
     <Provider store={store}>
       <App />
     </Provider>
@@ -29,7 +23,4 @@ ReactDOM.render(
   document.getElementById("root")
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
