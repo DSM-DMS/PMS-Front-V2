b60d314e58c9442547611a4cb6e64cc083e980f0
강은빈 <kub0803@naver.com>
2021-08-18 23:16:13 +0900

    [MODIFY] 메인 급식 카테고리 수정

--- src/components/MainPage/Category/Meals.js
+++ src/components/MainPage/Category/Meals.js
@@ -1,25 +1,25 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { FetchMeal } from "../../../utils/api/user";
-import { requestJW } from "../../../utils/axios/axios";
 import * as S from "../style";
 
 const btnLists = [{ id: 1 }, { id: 2 }, { id: 3 }];
 
 const Meals = () => {
   const [select, setSelect] = useState(1);
- 
+
   const date = new Date();
   const year = date.getFullYear();
   const month = String(date.getMonth() + 1).padStart(2, "0");
   const day = String(date.getDate()).padStart(2, "0");
 
-  let TodayDate = year + "" + month + "" + day;
+  let TodayDate = year + month + day;
 
   const mealBtnHandler = (list) => {
     setSelect(list.id);
   };
 
   const fetchMeal = FetchMeal(TodayDate);
+  console.log(TodayDate);
 
   return (
     <S.TodayMeals>
@@ -27,11 +27,21 @@ const Meals = () => {
         <S.Title>오늘의 급식</S.Title>
         <S.MealMenu>
           <ul>
-            {select === 1
-              ? fetchMeal?.breakfast.map((i, index) => <li key={index}>{i}</li>)
-              : select === 2
-              ? fetchMeal?.lunch.map((i, index) => <li key={index}>{i}</li>)
-              : fetchMeal?.dinner.map((i, index) => <li key={index}>{i}</li>)}
+            {fetchMeal?.breakfast[0] === "" ? (
+              <>급식이 없습니다.</>
+            ) : (
+              <>
+                {select === 1
+                  ? fetchMeal?.breakfast.map((i, index) => (
+                      <li key={index}>{i}</li>
+                    ))
+                  : select === 2
+                  ? fetchMeal?.lunch.map((i, index) => <li key={index}>{i}</li>)
+                  : fetchMeal?.dinner.map((i, index) => (
+                      <li key={index}>{i}</li>
+                    ))}
+              </>
+            )}
           </ul>
         </S.MealMenu>
         <S.MealButton>
