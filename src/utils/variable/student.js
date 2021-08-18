b0d50530bcd9b85b3a601cb90ae70d8e8924adcb
강은빈 <kub0803@naver.com>
2021-08-18 23:17:37 +0900

    [ADD] 글로벌 변수 추가

---
+++ src/utils/variable/student.js
@@ -0,0 +1,6 @@
+export const studentName = "student-name";
+export const studentNumber = "student-number";
+export const bonusPoint = "bonus-point";
+export const minusPoint = "minus-point";
+export const mealApplied = "meal-applied";
+export const stayStatus = "stay-status";
