3ad9b5474a84bd74614c61e73f07d68327fbf251
강은빈 <kub0803@naver.com>
2021-08-21 23:24:43 +0900

    [MODIFY] 학생 아이템 박스 수정

--- src/components/MainPage/Category/StudentItemBox.js
+++ src/components/MainPage/Category/StudentItemBox.js
@@ -1,25 +1,26 @@
 import React from "react";
 import { studentName } from "../../../utils/variable/student";
 import { Profile, Arrow } from "../../../assets";
-import * as S from "../style";
 
 const StudentItemBox = (props) => {
   return (
-    <div className="student-name-wrapper" key={props.index}>
-      <div className="student-name-info-wrapper">
-        <img className="profile-img" src={Profile} alt="프로필 사진" />
-        <div className="student-name">
-          <span>
-            {props.stdGrade}학년 {props.stdCls}반 {props.stdNum}번
-          </span>
-          <span>
-            소프트웨어개발과
-            {props.students[`${props.stdSelect}`]?.[`${studentName}`]}
-          </span>
+    <>
+      <div className="student-name-wrapper">
+        <div className="student-name-info-wrapper">
+          <img className="profile-img" src={Profile} alt="프로필 사진" />
+          <div className="student-name">
+            <span>
+              {props.stdGrade}학년 {props.stdCls}반 {props.stdNum}번
+            </span>
+            <span>
+              소프트웨어개발과
+              {props.user?.[`${props.stdSelect}`]?.[`${studentName}`]}
+            </span>
+          </div>
         </div>
+        <img className="arrow-img" src={Arrow} alt="화살표"></img>
       </div>
-      <img className="arrow-img" src={Arrow} alt="화살표"></img>
-    </div>
+    </>
   );
 };
 
