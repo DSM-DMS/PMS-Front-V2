8d7e7e0f4543cf1af89e90bdbd5456f1fb681211
LeeJinWoo <wlsdn5793@gmail.com>
2021-08-25 16:35:37 +0900

    [ADD] 마이페이지 학생 정보 추가

--- src/components/MyPage/ChildrenStatus/ChildrenStatus.js
+++ src/components/MyPage/ChildrenStatus/ChildrenStatus.js
@@ -1,68 +1,82 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import * as S from './style'
-import {Bike, Home, MealGreen} from '../../../assets/index'
+import {Bike, Home, MealGreen, MealRed, Remain } from '../../../assets/index'
 import ChildrenCurrentStatus from './ChildrenCurrentStatus/ChildrenCurrentStatus'
+import { StudentUser, StudentUserInfo } from '../../../utils/api/myPage'
 
 function ChildrenStatus(){
+    const studentUser = StudentUser();
+    const [studentNumber, setStudentNumber] = useState("");
+    useEffect(()=>{
+        setStudentNumber(studentUser?.students[0]?.['student-number']);
+    }, [studentUser?.students]);
+    const userInformation = StudentUserInfo(2202);
+
+    const [phrase, setPhrase] = useState(1);
+    const [previousPhrase, setPreviousPhrase] = useState(0);
+    const clickPhrase = (num) => {
+        setPreviousPhrase(phrase);
+        setPhrase(num);
+    }
     return(
         <S.ChildrenStatus>
             <S.StatusItemWrapper>
                 <S.ChildrenStatusItem>
-                    <S.ItemTitle>기숙사 생활</S.ItemTitle>
+                    <div className="itemTitle">기숙사 생활</div>
                     <S.ItemInnerWrapper>
-                        <S.CheckStatus>
+                        <div className="checkStatus">
                             <ul>
-                                <li>다벌점 봉사 활동 내역</li>
-                                <li>상벌점 내역 확인</li>
+                                <li onClick={()=>clickPhrase(1)} id="1">다벌점 봉사 활동 내역</li>
+                                <li onClick={()=>clickPhrase(2)} id="2">상벌점 내역 확인</li>
                             </ul>
-                        </S.CheckStatus>
-                        <S.ItemImgWrapper>
-                            <S.PrizePoint />
-                            <S.PenaltyPoint/>
-                        </S.ItemImgWrapper>
+                        </div>
+                        <div className="img-wrapper">
+                            <S.Point blue>{userInformation?.['bonus-point']}</S.Point>
+                            <S.Point>{userInformation?.['minus-point']}</S.Point>
+                        </div>
                     </S.ItemInnerWrapper>
                 </S.ChildrenStatusItem>
                 <S.ChildrenStatusItem>
-                    <S.ItemTitle>학교 생활</S.ItemTitle>
+                    <div className="itemTitle">학교 생활</div>
                         <S.ItemInnerWrapper>
-                            <S.CheckStatus>
+                            <div className="checkStatus">
                                 <ul>
-                                    <li>외출 내역 확인</li>
+                                    <li onClick={()=>clickPhrase(3)}>외출 내역 확인</li>
                                 </ul>
-                            </S.CheckStatus>
-                            <S.ItemImgWrapper>
+                            </div>
+                            <div className="img-wrapper">
                                 <img src={Bike} alt=""></img>
-                            </S.ItemImgWrapper>
+                            </div>
                         </S.ItemInnerWrapper>
                 </S.ChildrenStatusItem>
                 <S.ChildrenStatusItem>
-                    <S.ItemTitle>신청 상태</S.ItemTitle>
+                    <div className="itemTitle">신청 상태</div>
                     <S.ItemInnerWrapper>
-                        <S.CheckStatus>
+                        <div className="checkStatus">
                             <ul>
-                                <li>이번주 잔류 신청 상태</li>
-                                <li>주말 급식 신청 여부</li>
+                                <li onClick={()=>clickPhrase(4)}>이번주 잔류 신청 상태</li>
+                                <li onClick={()=>clickPhrase(5)}>주말 급식 신청 여부</li>
                             </ul>
-                        </S.CheckStatus>
-                        <S.ItemImgWrapper>
-                            <img src={Home} alt=""></img>
-                            <img src={MealGreen} alt=""></img>
-                        </S.ItemImgWrapper>
+                        </div>
+                        <div className="img-wrapper">
+                            <img src={userInformation?.['stay-status'] ? Remain : Home} alt="잔류여부"></img>
+                            <img src={userInformation?.['meal-applied'] ? MealGreen : MealRed} alt="급식신청여부"></img>
+                        </div>
                     </S.ItemInnerWrapper>
                 </S.ChildrenStatusItem>
                 <S.ChildrenStatusItem>
-                    <S.ItemTitle>동아리</S.ItemTitle>
+                    <div className="itemTitle">동아리</div>
                     <S.ItemInnerWrapper>
-                        <S.CheckStatus>
+                        <div className="checkStatus">
                             <ul>
                                 <li>DMS</li>
                             </ul>
-                        </S.CheckStatus>
-                        <S.ItemImgWrapper></S.ItemImgWrapper>
+                        </div>
+                        <div className="img-wrapper"></div>
                     </S.ItemInnerWrapper>
                 </S.ChildrenStatusItem>
             </S.StatusItemWrapper>
-            <ChildrenCurrentStatus />
+            <ChildrenCurrentStatus value={phrase}/>
         </S.ChildrenStatus>
     )
 }
